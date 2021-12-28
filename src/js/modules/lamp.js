3a7d6a454f46a28cebc7c28cd603e21a471673f9
admin <admin@admins-MacBook-Air.local>
2021-12-28 17:45:49 +0200

    workWithLamp

---
+++ src/js/modules/lamp.js
@@ -0,0 +1,15 @@
+const lamp = ()=>{
+    // const btn = document.querySelector('.btn');
+    
+    // setTimeout(() => {
+    //     document.querySelector('.wrapper').classList.toggle('_active');
+
+    //     setInterval(() => {
+    //         document.querySelector('.wrapper').classList.toggle('_active'); 
+    //     }, 800);
+
+    // }, 1100);
+
+
+};
+export default lamp;
